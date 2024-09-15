a139b291c4016d4804afe17f80a5641d50877588
Saumya Pandey <saumyap271@gmail.com>
2024-09-15 12:38:15 +0530

    Update Bubblesort.cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,8 +1,8 @@
  #include<bits/stdc++.h>
  using namespace std ;
  void Bubble_sort(int arr[],int n){
-    for(int i=0; i>=0; i--){
-        for(int j=i; j<=i-1; j++){
+    for(int i=n-1; i>=0; i--){
+        for(int j=0; j<=i-1; j++){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j+1];
                 arr[j+1]=arr[j];
