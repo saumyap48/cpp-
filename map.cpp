f2c37ec1bb14914ab414e1c3c7c9790949ef5a19
Saumya Pandey <saumyap271@gmail.com>
2024-10-03 19:10:07 +0530

    Add files via upload

---
+++ map.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0; i<n; i++){
+    cin>> arr[i];
+  }
+  map<int,int>mpp;
+  for(int i=0; i<n; i++){
+    mpp[arr[i]]++;
+  }
+  int q;
+  cin>>q;
+  while(q--){
+    int number;
+    cin>>number;
+    cout<<mpp[number]<<endl;
+  }
+  return 0;
+}
