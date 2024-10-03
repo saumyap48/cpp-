25c6ed29cc3c9db0ac2dcb7a22dd79e14e98b7e4
Saumya Pandey <saumyap271@gmail.com>
2024-10-03 12:54:35 +0530

    Add files via upload

---
+++ HashingString.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;
+  cin>>s;
+  
+  int hash[26]={0};
+  for(int i=0; i<s.size(); i++){
+    hash[s[i]-'a']++;
+  }
+  int q;
+  cin>>q;
+  while(q--){
+   char c;
+   cin>>c;
+   cout<<hash[c-'a']<<endl;
+  }
+  return 0;
+}
