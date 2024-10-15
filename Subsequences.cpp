5e22b4862525fdcb2d6ebb832080f825eaefd4b7
Saumya Pandey <saumyap271@gmail.com>
2024-10-15 17:18:41 +0530

    Add files via upload

---
+++ Subsequences.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printS(int inde,vector<int>&ds,int s,int sum,vector<int>&arr,int n){
+    if(inde==n){
+        if(s==sum){
+            for(int num: ds)
+            cout<<num<<" ";
+            cout<<endl;
+        }
+        return;
+}
+ds.push_back(arr[inde]);
+s+=arr[inde];
+printS(inde+1,ds,s,sum,arr,n);
+s-=arr[inde];
+ds.pop_back();
+printS(inde+1,ds,s,sum,arr,n);
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int sum;
+    cin>>sum;
+    vector<int>ds;
+    printS(0,ds,0,sum,arr,n);
+    return 0;
+}
