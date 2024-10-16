7f0d09da43c7604bea098e20e20249df01483e72
Saumya Pandey <saumyap271@gmail.com>
2024-10-16 13:27:41 +0530

    Add files via upload

---
+++ DynamicLinkedlist.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+int data;
+Node*next;
+ Node(int value){
+    data=value;
+    next=NULL;
+}
+};
+int main(){
+  Node *Head;
+  Head=new Node(4);
+  cout<<Head->data<<endl;
+  cout<<Head->next<<endl;
+}
