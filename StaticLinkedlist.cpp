15a52da22c35610ddb9a21e55d8a9ab191969f64
Saumya Pandey <saumyap271@gmail.com>
2024-10-16 13:27:18 +0530

    Add files via upload

---
+++ StaticLinkedlist.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+int data;
+Node*next;
+};
+int main(){
+  Node A1;
+  A1.data=4;
+  A1.next=NULL;
+  cout<<A1.data<<endl;
+  cout<<A1.next<<endl;
+}
