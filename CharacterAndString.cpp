3d1294540fd17c2e34694e05e20058c18913515e
Saumya Pandey <saumyap271@gmail.com>
2024-10-21 19:52:29 +0530

    Add files via upload

---
+++ CharacterAndString.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+ /* 
+  char arr[]={'a','p','p','l','e'};
+  for(int i=0; i<5; i++){
+  cout<<arr[i];
+    }
+  
+  char arr[10];
+  cin>>arr;
+  cout<<arr;
+  
+  only print up to 9 character only the last is null character
+  
+  terminated after a null character
+  char arr[10];
+  cin>>arr;
+  arr[2]='\0';
+  cout<<arr;
+ 
+  
+
+ string s="Sohit";
+ cout<<s;
+ 
+ 
+ string s;
+ cin>>s;
+ cout<<s;
+
+ 
+
+ string s;
+ getline(cin,s);
+ cout<<s;
+ cout<<s.size();
+
+ 
+ 
+ string s1="sohit",
+ s2="pohit";
+ string s3=s1+s2;
+ cout<<s3;
+ 
+ string s1="sohit", s2="pohit ";
+ s1.push_back('p');
+ s1=s1+"paaaa";
+ cout<<s1;
+ 
+
+ //escape character
+
+ string s = "rohit negi is a \"good\" boy";
+ cout<<s;
+ 
+
+ string s="\\0";
+ cout<<s;
+ */
+}
