27829e534851691f8a64c6a4544d274ac790c320
Saumya Pandey <saumyap271@gmail.com>
2024-11-15 12:18:17 +0530

    Update Quicksort.cpp

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -43,3 +43,31 @@ int main(){
     cout<<"\n";
     return 0;
     }
+//little different
+#include<bits/stdc++.h>
+using namespace std;
+int partition(int arr[], int start,int end){
+  int pos=start;
+  for(int i=start; i<=end; i++){
+    if(arr[i]<=arr[end]){
+      swap(arr[i],arr[pos]);
+      pos++;
+    }
+  }
+  return pos-1;
+}
+void quicksort(int arr[], int start,int end){
+  if(start>=end)
+  return ;
+  
+   int pivot=partition(arr,start,end);
+   quicksort(arr,start,pivot-1);
+   quicksort(arr,pivot,end);
+}
+int main(){
+  int arr[]={10,3,4,1,5,6,3,2,11,9};
+  quicksort(arr,0,9);
+  for(int i=0; i<10; i++){
+    cout<<arr[i]<<" ";
+  }
+}
