43e7c38ac6e3747dabe99766aa29e6935f1a3aa7
Saumya Pandey <saumyap271@gmail.com>
2024-11-15 08:34:55 +0530

    Update Megresort.cpp

--- Megresort.cpp
+++ Megresort.cpp
@@ -53,3 +53,51 @@ int main(){
     cout<<endl;
     return 0;
 }
+
+ //now a little different method
+
+#include<bits/stdc++.h>
+using namespace std;
+void merge(int arr[], int start, int mid, int end){
+    vector<int>temp(end-start+1);
+    int left=start,right=mid+1;
+    int index=0;
+    while(left<=mid && right<=end){
+        if(arr[left]<=arr[right]){
+            temp[index]=arr[left];
+            index++,left++;
+        }
+        else{
+            temp[index]=arr[right];
+            index++,right++;
+        }
+    }
+    while(left<=mid){
+        temp[index]=arr[left];
+        index++,left++;
+    }
+    while(right<=end){
+        temp[index]=arr[right];
+        index++,right++;
+    }
+     index=0;
+    while(start<=end){
+        arr[start]=temp[index];
+        start++,index++;
+    }
+}
+void mergesort(int arr[], int start,int end){
+    if (start==end)
+        return;
+   int mid=start+(end-start)/2;
+   mergesort(arr,start,mid);
+   mergesort(arr,mid+1,end);
+   merge(arr,start,mid,end);
+}
+int main(){
+    int arr[]={9,2,3,4,0,5,3,0,9,5};
+    mergesort(arr,0,9);
+    for(int i=0; i<10; i++){
+        cout<<arr[i]<<" ";
+    }
+}
